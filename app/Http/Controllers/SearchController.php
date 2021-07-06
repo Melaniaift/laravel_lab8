2752d97f7c705fa56bff53bd4faaa4cf18b7f4b5
Melaniaift <ioanamelany99@yahoo.com>
2021-07-06 16:59:07 +0300

    laravellab8

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Flower;
+
+class SearchController extends Controller
+{
+   public function index(){
+       return view('flowers.search');
+   }
+   public function search(Request $request)
+    {
+        $search_term=$request->get('search');
+        $flowers=Flower::where('nume', $search_term)-> orwhere('culoare',$search_term)-> orwhere('marime',$search_term)->get();
+        return view('flowers.search_result')->with('flowers', $flowers);
+    }
+}
